74a1c8c772c7ecefd85932ced31a0c9d343dc077
IlyaDemidov <idemidov92@yandex.ru>
2023-02-15 23:13:38 +0300

    ДЗ № 1.2

---
+++ 1. Новшества С++/Задача 2. std_variant/Task_1.2.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+#include <variant>
+#include <vector>
+#include <algorithm>
+
+std::variant<int, std::string, std::vector<int>> get_variant() 
+{
+	std::srand(std::time(nullptr));
+	int random_variable = std::rand() % 3;
+
+	std::variant<int, std::string, std::vector<int>> result;
+	switch (random_variable)
+	{
+	case 0:
+		result = 5;
+		break;
+	case 1:
+		result = "string";
+		break;
+	case 2:
+		result = std::vector<int>{ 1, 2, 3, 4, 5 };
+		break;
+	default:
+		break;
+	}
+	return result;
+}
+
+void foo(std::variant <int, std::string, std::vector<int>> x)
+{
+	if(std::holds_alternative<int>(x)) std::cout << std::get<int>(x)*2 << "\n";
+	if (std::holds_alternative<std::string>(x)) std::cout << std::get<std::string>(x) << "\n";
+	if (std::holds_alternative<std::vector<int>>(x)) std::ranges::for_each(std::get<std::vector<int>>(x), [](const int& i) {std::cout << i << " "; }) ;
+}
+
+int main()
+{
+	foo(get_variant());
+    
+	std::cout << "\n";
+	system("pause");
+	return 0;
+}
