471c949e2f0d20d9e8dfb42b2fa6fc8b25310b83
IlyaDemidov <idemidov92@yandex.ru>
2023-02-15 23:12:33 +0300

    ДЗ № 1.1

---
+++ 1. Новшества С++/Задача 1. Лямбда-функция/Task_1.1.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <Windows.h>
+#include <vector>
+#include <algorithm>
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    std::vector<int> arr{ 4, 7, 9, 14, 12 };
+
+    auto my_print = [](const int& i) {std::cout << i << " "; };
+
+    std::cout << "Входные данные: ";
+    std::ranges::for_each(arr, my_print);
+
+    
+    std::ranges::for_each(arr, [](int& n) {if(n % 2 != 0) n*=3;});
+
+    std::cout << "\nВыходные данные: ";
+    std::ranges::for_each(arr, my_print);
+
+    std::cout << "\n";
+    system("pause");
+
+    return 0;
+}
