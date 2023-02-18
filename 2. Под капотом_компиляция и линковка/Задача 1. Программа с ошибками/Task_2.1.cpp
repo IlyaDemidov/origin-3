a3e6ee425fa77565121649a463d24fde37ab9542
IlyaDemidov <idemidov92@yandex.ru>
2023-02-18 17:57:44 +0300

    ДЗ № 2.1

---
+++ 2. Под капотом_компиляция и линковка/Задача 1. Программа с ошибками/Task_2.1.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+
+struct Point
+{
+	double m_x;
+	double m_y;
+	Point(double x, double y)
+	{
+		m_x = x;
+		m_y = y;
+	}
+};
+
+void print_point(const Point& point_object)
+{
+	std::cout << "x:" << point_object.m_x << ", y: "
+		<< point_object.m_y << std::endl;
+}
+
+int main()
+{
+	for (int i = 0; i < 5; i++) 
+	{
+		Point my_point(i, 2 * i);
+		print_point(my_point);
+	}
+
+	std::cout << "\n";
+	system("pause");
+	return 0;
+}
