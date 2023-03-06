ea9d43a73061fe5d23b4682b0933ad1106dba3ec
IlyaDemidov <idemidov92@yandex.ru>
2023-03-06 21:53:13 +0300

    ДЗ № 5.3

---
+++ 5. Шаблоны и функторы/Задача 3. Счётчик чисел/Task_5.3.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+class Number_Counter
+{
+public:
+    Number_Counter() :sum{ 0 }, count{0} {}
+	
+    const int& get_sum() const { return sum; }
+    const int& get_count() const { return count; }
+
+    void operator () (const std::vector<int>& vec)
+    {
+        std::for_each(vec.begin(), vec.end(), [this](const int& x) {if (x % 3 == 0) { set_count(); set_sum(x); } });
+    }
+
+private:
+    int sum;
+    int count;
+
+    void set_count() { count++; }
+    void set_sum(const int& x) { sum += x; }
+
+};
+
+
+int main()
+{
+    Number_Counter c;
+
+    std::vector<int> vec{ 4, 1, 3, 6, 25, 54 };
+
+    c(vec);
+
+    std::cout << "[IN]: ";
+    for (const int& i : vec) std::cout << i << " ";
+
+    std::cout << "\n[OUT]: get_sum() = " << c.get_sum() << '\n';
+    std::cout << "[OUT]: get_count() = " << c.get_count() << '\n';
+
+
+
+    std::cout << "\n";
+    system("pause");
+    return 0;
+}
