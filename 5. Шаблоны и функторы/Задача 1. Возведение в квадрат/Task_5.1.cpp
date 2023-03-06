42edaf54174e61dae25fddb2d02a9a9be8530b4a
IlyaDemidov <idemidov92@yandex.ru>
2023-03-06 21:52:16 +0300

    ДЗ № 5.1

---
+++ 5. Шаблоны и функторы/Задача 1. Возведение в квадрат/Task_5.1.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <vector>
+
+template <typename T>
+void my_sgr(T x)
+{
+    std::cout << "[IN]: " << x << "\n";
+    std::cout << "[OUT]: " << x*x << "\n";
+}
+
+template<>
+void my_sgr(std::vector<int> x)
+{
+    std::cout << "[IN]: ";
+    for (int i = 0; i < x.size(); i++)
+    {
+        if (i == x.size() - 1) {std::cout << x[i] << std::endl; break;}
+        std::cout << x[i] << ", ";
+    }
+
+    std::cout << "[OUT]: ";
+    for (int i = 0; i < x.size(); i++)
+    {
+        if (i == x.size() - 1) { std::cout << x[i] * x[i] << std::endl; break; }
+        std::cout << x[i] * x[i] << ", ";
+    }
+}
+
+int main()
+{
+    my_sgr(4);
+    my_sgr(std::vector<int>{ -1, 4, 8 });
+        
+      
+    std::cout << "\n";
+    system("pause");
+    return 0;
+}
