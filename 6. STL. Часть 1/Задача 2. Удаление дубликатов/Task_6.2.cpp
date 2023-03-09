530f0fb6cd15ac2309793e0dd9fd5194ccf4a06a
IlyaDemidov <idemidov92@yandex.ru>
2023-03-09 22:59:33 +0300

    ДЗ № 6.2

---
+++ 6. STL. Часть 1/Задача 2. Удаление дубликатов/Task_6.2.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <set>
+#include <conio.h>
+
+int main()
+{
+	std::set<int> list;
+	int Size{ 0 };
+	
+	std::cout << "[IN]:" << "\n";
+	std::cin >> Size;
+
+	for (size_t i = 0; i < Size; i++)
+	{
+		int x{ 0 }; std::cin >> x;
+		list.insert(x);
+	}
+
+	std::cout << "[OUT]:" << "\n";
+	for (auto it = list.rbegin(); it != list.rend(); ++it)
+		std::cout << *it << '\n';
+
+	
+	std::cout << "\n";
+	system("pause");
+	return 0;
+}
