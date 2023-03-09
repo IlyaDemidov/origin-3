004c55ffaf0a36622109d4f95bd9541aa72789ee
IlyaDemidov <idemidov92@yandex.ru>
2023-03-09 22:58:45 +0300

    ДЗ № 6.1

---
+++ 6. STL. Часть 1/Задача 1. Частота символов/Task_6.1.cpp
@@ -0,0 +1,31 @@
+﻿#include <map>
+#include <algorithm>
+#include <string>
+#include <iostream>
+
+void sort(std::map< char, int>& m)
+{
+    std::multimap<int, char> multi_m;
+
+    for (auto& i : m) { multi_m.insert({ i.second, i.first });}
+        
+    for (auto it = multi_m.rbegin(); it != multi_m.rend(); ++it)
+        std::cout << it->second << ": " << it->first << '\n';
+}
+
+int main()
+{
+    std::string str = "Hello world!!";
+
+    std::map<char, int> freq;
+    std::for_each(std::begin(str),std::end(str),[&freq](const char& c) {freq[c]++;});
+
+    std::cout << "[IN]: " << str << std::endl;
+    std::cout << "[OUT]:" << std::endl;
+
+    sort(freq);
+    
+    std::cout << "\n";
+    system("pause");
+    return 0;
+}
