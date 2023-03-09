b51c8d42ef0a4480ac6fc2e91499f56170c6fad8
IlyaDemidov <idemidov92@yandex.ru>
2023-03-09 07:37:22 +0300

    Update Task_5.1.cpp

--- 5. Шаблоны и функторы/Задача 1. Возведение в квадрат/Task_5.1.cpp
+++ 5. Шаблоны и функторы/Задача 1. Возведение в квадрат/Task_5.1.cpp
@@ -1,37 +1,44 @@
 ﻿#include <iostream>
 #include <vector>
 
-template <typename T>
-void my_sgr(T x)
+void print_vector(std::vector<int> x)
 {
-    std::cout << "[IN]: " << x << "\n";
-    std::cout << "[OUT]: " << x*x << "\n";
+  for (int i = 0; i < x.size(); i++)
+  {
+    if (i == x.size() - 1) {std::cout << x[i] << std::endl; break;}
+    std::cout << x[i] << ", ";
+  }
+}
+
+template <typename T>
+T my_sgr(T x)
+{
+    return x * x;
 }
 
 template<>
-void my_sgr(std::vector<int> x)
+std::vector<int> my_sgr(std::vector<int> x)
 {
-    std::cout << "[IN]: ";
-    for (int i = 0; i < x.size(); i++)
-    {
-        if (i == x.size() - 1) {std::cout << x[i] << std::endl; break;}
-        std::cout << x[i] << ", ";
-    }
-
-    std::cout << "[OUT]: ";
-    for (int i = 0; i < x.size(); i++)
-    {
-        if (i == x.size() - 1) { std::cout << x[i] * x[i] << std::endl; break; }
-        std::cout << x[i] * x[i] << ", ";
-    }
+    std::vector<int>y;
+    for (const int& i : x) { y.push_back(i * i); }
+    return y;
 }
 
 int main()
 {
-    my_sgr(4);
-    my_sgr(std::vector<int>{ -1, 4, 8 });
-        
+    int var{ 4 };
+    std::cout << "[IN]: " << var << "\n";
+    std::cout << "[OUT]: " << my_sgr(var) << "\n";
+    
+    std::vector<int>vec{ -1, 4, 8 };
+    std::cout << "[IN]: ";
+    print_vector(vec);
+    
+    std::cout << "[OUT]: ";
+    print_vector(my_sgr(vec));
       
+
+
     std::cout << "\n";
     system("pause");
     return 0;
