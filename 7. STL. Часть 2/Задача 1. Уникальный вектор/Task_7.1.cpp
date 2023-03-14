8c27787b14f400a295f37cbabf01429ae7723fec
IlyaDemidov <idemidov92@yandex.ru>
2023-03-14 07:26:15 +0300

    ДЗ № 7.1

---
+++ 7. STL. Часть 2/Задача 1. Уникальный вектор/Task_7.1.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+void killer_doubles(std::vector<int>& vec)
+{
+    std::sort(vec.begin(),vec.end());
+    auto v = std::unique(vec.begin(), vec.end());
+    vec.erase(v, vec.end());
+}
+
+void print_vec(const std::vector<int>& vec)
+{
+    for (const auto& i : vec) std::cout << i << " ";
+
+    std::cout << '\n';
+}
+
+int main()
+{
+    std::vector<int> vec{ 1, 1, 2, 5, 6, 1, 2, 4 };
+
+    std::cout << "[IN]: "; 
+    print_vec(vec);
+
+    killer_doubles(vec);
+
+    std::cout << "[OUT]: ";
+    print_vec(vec);
+
+
+
+    std::cout << "\n";
+    system("pause");
+    return 0;
+}
