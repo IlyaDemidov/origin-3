a253b85ea26977e9e721a27584f80d4ccf666a36
IlyaDemidov <idemidov92@yandex.ru>
2023-03-14 07:27:05 +0300

    ДЗ № 7.2

---
+++ 7. STL. Часть 2/Задача 2. Печать контейнера/Task_7.2.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <set>
+#include <vector>
+#include <list>
+
+
+template <typename T>
+void print_container(const T& t)
+{
+    for (const auto& i : t) {
+        std::cout << i << " ";
+    }
+    std::cout << std::endl;
+}
+
+
+int main()
+{
+    std::set<std::string> test_set = { "one", "two", "three", "four" };
+    print_container(test_set);
+
+    std::list<std::string> test_list = { "one", "two", "three", "four" };
+    print_container(test_list);
+
+    std::vector<std::string> test_vector = { "one", "two", "three", "four" };
+    print_container(test_vector);
+    
+    
+    
+    std::cout << "\n";
+    system("pause");
+    return 0;
+}
